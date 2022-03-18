59ad66e3b6eb2d28fd5def52bd193da03209b931
Sanamwol <101869933+Sanamwol@users.noreply.github.com>
2022-03-18 17:08:43 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -54,7 +54,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
             var uri = encodeURI(match[1])
         let banned = jid.find( Jid => Jid === message.jid);
         if(banned !== undefined) return
-        if (!!message.mention && message.mention[0] == '919895809960@s.whatsapp.net') {
+        if (!!message.mention && message.mention[0] == '916238329488@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./files/uploads/p3r3.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
         if (!!message.mention && message.mention[0] == Config.MENTION) {
